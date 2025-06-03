afc5a0f510273e8c137d56484eec670bbe54cdb3
Esteban <evbandev@outlook.com>
2025-06-02 20:48:10 -0400

    Correction to the single img and demo link

--- single.php
+++ single.php
@@ -3,9 +3,7 @@
   <section class="proyect-section-content">
     <a class="proyect-section-content__return-link global-project-icon-link" href="<?php echo esc_url(home_url('/')); ?>"><i class="bi bi-arrow-left-short"></i> Volver al inicio</a>
     <h1 class="global-main-title"><?php the_title(); ?></h1>
-    <?php if (get_field('imagen_del_proyecto')): ?>
-      <img class="proyect-section-content__image" src="<?php the_field('imagen_del_proyecto'); ?>" alt="<?php the_title(); ?>" />
-    <?php endif; ?>
+    <?php the_post_thumbnail('full', ['class' => 'proyect-section-content__image']); ?>
     <div class="projects-section-container__tag-container">
       <?php the_field('etiquetas_del_proyecto'); ?>
     </div>
@@ -13,7 +11,7 @@
       <?php echo wp_kses_post(get_field('descripcion_del_proyecto')); ?>
     </p>
     <div class="proyect-section-content__links">
-      <a class="global-button" href="<?php echo esc_attr(get_field('link_a_demo_del_proyecto')); ?>"><?php echo esc_html('Ver Demo del proyecto'); ?></a>
+      <a class="global-button" href="<?php echo esc_attr(get_field('link_a_demo_del_proyecto')); ?>" target="_blank"><?php echo esc_html('Ver Demo del proyecto'); ?></a>
       <a
         class="global-icon-link global-project-icon-link"
         href="<?php echo esc_attr(get_field('link_al_codigo_del_proyecto')); ?>"
